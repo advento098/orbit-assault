ded02ad7b37e6e84e767af4b13c2fdb7c16b6a99
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-07-21 16:06:49 +0800

    Added a neon title

--- orbit-assault/src/App.jsx
+++ orbit-assault/src/App.jsx
@@ -2,16 +2,27 @@ import { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const videoRef = useRef(null);
-
-  useEffect(() => {}, []);
-
   return (
     <div>
-      <video ref={videoRef} autoPlay loop muted>
+      <video autoPlay loop muted>
         <source src="./src/assets/space-background.mp4" type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
+
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+
+      <section>
+        <h1 className="poppins-regular">
+          ORBIT <br />
+          <span className="poppins-bold">ASSAULT</span>
+        </h1>
+      </section>
+      <section></section>
     </div>
   );
 }
